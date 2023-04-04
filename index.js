708e9063f2db5ac5c6d5a55a9ec2015a7d680b6b
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-04 17:05:09 -0400

    Wrote tests and created example logo

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const inquirer = require('inquirer');
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 const fs = require('fs');
 const { default: Choices } = require('inquirer/lib/objects/choices');
 const { default: Choice } = require('inquirer/lib/objects/choice');
 
 const {Shape, Circle, Rectangle, Triangle, Square} = require("./lib/shapes");
+
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 const questions = [
     {
-        type: "input",
+        type: "maxlength-input",
         message: "What text would you like? (Up to 3 characters)",
         name: "text",
         maxLength: 3
